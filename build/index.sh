0734ae181db7b9f876425e281addd8c606e60c3c
Jannis R <mail@jannisr.de>
2015-08-25 01:26:57 +0200

    `build/`: moved `03-simplify.coffee` to `03-schedules.coffee`

--- build/index.sh
+++ build/index.sh
@@ -2,5 +2,5 @@
 
 ./node_modules/.bin/coffee ./build/01-download.coffee
 ./node_modules/.bin/coffee ./build/02-csv-ndjson.coffee
-./node_modules/.bin/coffee ./build/03-simplify.coffee
+./node_modules/.bin/coffee ./build/03-schedules.coffee
 ./node_modules/.bin/coffee ./build/04-cleanup.coffee
